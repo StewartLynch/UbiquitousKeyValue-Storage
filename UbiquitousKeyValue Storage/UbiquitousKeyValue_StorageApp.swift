9a2530199532269c2a7df671ed8c5fb87635a0f5
Stewart Lynch <slynch@createchsol.com>
2021-10-24 22:16:38 -0700

    Initial Commit

---
+++ UbiquitousKeyValue Storage/UbiquitousKeyValue_StorageApp.swift
@@ -0,0 +1,17 @@
+//
+//  UbiquitousKeyValue_StorageApp.swift
+//  UbiquitousKeyValue Storage
+//
+//  Created by Stewart Lynch on 2021-10-24.
+//
+
+import SwiftUI
+
+@main
+struct UbiquitousKeyValue_StorageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
